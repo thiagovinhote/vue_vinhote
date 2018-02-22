078fa26f237c7fa08e54d8e020aeffe868b53c5d
Thiago Vinhote <thiagovf26@outlook.com>
2018-02-22 01:50:49 -0400

    Página NotFound
    Signed-off-by: Thiago Vinhote <thiagovf26@outlook.com>

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="container">
-    Not Found
-  </div>
+  <Zone size="is-large has-text-centered is-light">
+    <h1 class="title is-1">404</h1>
+    <h1 class="title">Página não encontrada</h1>
+    <p>
+      Ir para o <strong><router-link to="/" class="has-text-info">início</router-link></strong>
+    </p>
+  </Zone>
 </template>
 
 <script>
-export default {};
+import Zone from '@/components/Zone';
+
+export default {
+  components: {
+    Zone,
+  },
+};
 </script>
 
 <style></style>
